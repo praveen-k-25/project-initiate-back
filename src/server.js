342a921baf5a334a59be9803c8a255479c13fd92
praveen k <119788504+praveen-k-25@users.noreply.github.com>
2025-10-02 13:02:58 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const {connectDB} = require("./database/db");
-const {globalErrorHandler} = require("./middleware/ErrorHandler");
+const { connectDB } = require("./database/db");
+const { globalErrorHandler } = require("./middleware/ErrorHandler");
 const userRouter = require("./routes/userRoutes");
 const app = express();
 
@@ -12,13 +12,20 @@ connectDB();
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:4000", "http://localhost:5173"],
+    origin: [
+      "http://localhost:4000",
+      "http://localhost:5173",
+      "https://7hq5fgv5-5173.inc1.devtunnels.ms/",
+    ],
+    credentials: true,
   })
 );
+
 app.use(express.json());
 
 // routes
 app.use("/user", userRouter);
+app.use("/server", userRouter);
 
 // captures all errors
 app.use(globalErrorHandler);

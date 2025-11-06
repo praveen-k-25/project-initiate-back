e0439c010851b10de18c08bea09f05451b1411b0
SWTDeveloper <sproutwingsdeveloper@gmail.com>
2025-11-06 17:57:08 +0530

    self ping every 10 min and port changes to 10000

--- src/utils/keepserverAwake.js
+++ src/utils/keepserverAwake.js
@@ -8,7 +8,7 @@ function keepServerAwake() {
     } catch (error) {
       console.error("🔴 Self-ping failed:", error.message);
     }
-  }, 1000); // every 9 minutes
+  }, 10 * 60 * 1000); // every 9 minutes
 }
 
 module.exports = keepServerAwake;
